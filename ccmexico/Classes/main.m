7b6285d5466b5e6b9acae5a814debd53ae92ae5e
Pau Ochoa <Pau8av@gmail.com>
2011-09-19 16:51:43 -0500

    Code: Part 1 of 2

--- ccmexico/Classes/main.m
+++ ccmexico/Classes/main.m
@@ -1,3 +1,9 @@
+//
+//  main.m
+//  ccmexico
+//
+//  Created by Pau Ochoa on 26/05/11.
+//  
 //  Copyright (C) 2011  Paulino Ochoa/Alternativa Digital
 //
 //  This program is free software: you can redistribute it and/or modify
@@ -16,3 +22,28 @@
 //  Contact:
 //  paulino@adigital.com.mx
 //  @pau_ochoa
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  /*
+  if ( [[NSUserDefaults standardUserDefaults] objectForKey:@"language"] == nil ) 
+  {
+    [[NSUserDefaults standardUserDefaults] setObject:[NSArray arrayWithObject:@"es"] forKey:@"AppleLanguages"];
+  } 
+  else
+  {
+    [[NSUserDefaults standardUserDefaults] setObject:[NSArray arrayWithObject:[[NSUserDefaults standardUserDefaults] objectForKey:@"language"]] forKey:@"AppleLanguages"];
+  }
+    */
+  
+    
+  
+  int retVal = UIApplicationMain(argc, argv, nil, nil);
+  [pool release];
+
+  
+  return retVal;
+}
